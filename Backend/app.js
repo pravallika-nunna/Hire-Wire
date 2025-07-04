378542fbc6e1588e7f65890b433441c3a60333f3
26thrishitha <thrishitha26@gmail.com>
2025-07-04 18:15:16 +0530

    newLetter enabled

--- Backend/app.js
+++ Backend/app.js
@@ -36,7 +36,7 @@ app.use("/api/v1/user",userRouter);
 app.use("/api/v1/job",jobRouter);
 app.use("/api/v1/application",applicationRouter);
 
-// newsLetterCron();
+newsLetterCron();
 
 connection();
 
